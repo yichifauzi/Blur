a520948fff11ec4156999739b7b3fb1267041160
Motschen <motschenfm@gmail.com>
2021-03-05 17:52:48 +0100

    Blur 2.1.0 - MidnightConfig
    Switch to MidnightConfig:
    - Much smaller file size
    - Live preview
    - Fixed #4
    
    Chinise translation by @Enaium
    Update to 21w08b

--- src/main/java/com/tterrag/blur/Blur.java
+++ src/main/java/com/tterrag/blur/Blur.java
@@ -1,12 +1,11 @@
 package com.tterrag.blur;
 
 import com.tterrag.blur.config.BlurConfig;
+import com.tterrag.blur.config.MidnightConfig;
 import ladysnake.satin.api.event.ShaderEffectRenderCallback;
 import ladysnake.satin.api.managed.ManagedShaderEffect;
 import ladysnake.satin.api.managed.ShaderEffectManager;
 import ladysnake.satin.api.managed.uniform.Uniform1f;
-import me.sargunvohra.mcmods.autoconfig1u.AutoConfig;
-import me.sargunvohra.mcmods.autoconfig1u.serializer.JanksonConfigSerializer;
 import net.fabricmc.api.ClientModInitializer;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
@@ -14,7 +13,6 @@ import net.minecraft.util.Identifier;
 import org.apache.commons.lang3.ArrayUtils;
 
 public class Blur implements ClientModInitializer {
-    public static BlurConfig BLUR_CONFIG;
 
     public static final String MODID = "blur";
 
@@ -22,15 +20,14 @@ public class Blur implements ClientModInitializer {
     public int colorFirst, colorSecond;
 
     private final ManagedShaderEffect blur = ShaderEffectManager.getInstance().manage(new Identifier(MODID, "shaders/post/fade_in_blur.json"),
-            shader -> shader.setUniformValue("Radius", (float) getRadius()));
+            shader -> shader.setUniformValue("Radius", BlurConfig.radius));
     private final Uniform1f blurProgress = blur.findUniform1f("Progress");
 
     public static final Blur INSTANCE = new Blur();
 
     @Override
     public void onInitializeClient() {
-        AutoConfig.register(BlurConfig.class, JanksonConfigSerializer::new);
-        BLUR_CONFIG = AutoConfig.getConfigHolder(BlurConfig.class).getConfig();
+        MidnightConfig.init("blur", BlurConfig.class);
 
         ShaderEffectRenderCallback.EVENT.register((deltaTick) -> {
             if (start > 0) {
@@ -43,12 +40,12 @@ public class Blur implements ClientModInitializer {
     private boolean doFade = false;
     public void onScreenChange(Screen newGui) {
         if (MinecraftClient.getInstance().world != null) {
-            boolean excluded = newGui == null || ArrayUtils.contains(BLUR_CONFIG.blurExclusions, newGui.getClass().getName());
+            boolean excluded = newGui == null || ArrayUtils.contains(BlurConfig.blurExclusions, newGui.getClass().getName());
             if (!excluded) {
-                blur.setUniformValue("Radius", (float) getRadius());
-                colorFirst = Integer.parseUnsignedInt(BLUR_CONFIG.gradientStartColor, 16);
-                colorSecond = Integer.parseUnsignedInt(BLUR_CONFIG.gradientEndColor, 16);
-                if (doFade == true) {
+                blur.setUniformValue("Radius", BlurConfig.radius);
+                colorFirst = Integer.parseUnsignedInt(String.valueOf(BlurConfig.gradientStartColor), 16);
+                colorSecond = Integer.parseUnsignedInt(String.valueOf(BlurConfig.gradientEndColor), 16);
+                if (doFade) {
                     start = System.currentTimeMillis();
                     doFade = false;
                 }
@@ -59,12 +56,8 @@ public class Blur implements ClientModInitializer {
         }
     }
 
-    public int getRadius() {
-        return BLUR_CONFIG.radius;
-    }
-
     private float getProgress() {
-        return Math.min((System.currentTimeMillis() - start) / (float) BLUR_CONFIG.fadeTimeMillis, 1);
+        return Math.min((System.currentTimeMillis() - start) / (float) BlurConfig.fadeTimeMillis, 1);
     }
 
     public int getBackgroundColor(boolean second) {
--- src/main/java/com/tterrag/blur/config/BlurConfig.java
+++ src/main/java/com/tterrag/blur/config/BlurConfig.java
@@ -1,16 +1,16 @@
 package com.tterrag.blur.config;
 
-import me.sargunvohra.mcmods.autoconfig1u.ConfigData;
-import me.sargunvohra.mcmods.autoconfig1u.annotation.Config;
-import me.sargunvohra.mcmods.autoconfig1u.annotation.ConfigEntry;
 import net.minecraft.client.gui.screen.ChatScreen;
 
-@Config(name = "blur")
-public class BlurConfig implements ConfigData {
-    @ConfigEntry.Gui.Excluded
-    public String[] blurExclusions = new String[]{ ChatScreen.class.getName() };
-    public int fadeTimeMillis = 200;
-    public int radius = 8;
-    public String gradientStartColor = "75000000";
-    public String gradientEndColor = "75000000";
+public class BlurConfig extends MidnightConfig {
+    @Entry
+    public static String[] blurExclusions = new String[]{ ChatScreen.class.getName() };
+    @Entry(min = 0, max = 5000)
+    public static int fadeTimeMillis = 200;
+    @Entry(min = 0, max = 500)
+    public static int radius = 8;
+    @Entry(min = 0, max = 99999999)
+    public static int gradientStartColor = 75000000;
+    @Entry(min = 0, max = 99999999)
+    public static int gradientEndColor = 75000000;
 }
