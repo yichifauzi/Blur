1260881f41148f8df69b05e92aa00daa4d140a40
Motschen <motschenfm@gmail.com>
2021-12-04 11:42:13 +0100

    Update to 1.18

--- src/main/java/com/tterrag/blur/Blur.java
+++ src/main/java/com/tterrag/blur/Blur.java
@@ -33,6 +33,7 @@ public class Blur implements ClientModInitializer {
         defaultExclusions.add(ChatScreen.class.getName());
         defaultExclusions.add("com.replaymod.lib.de.johni0702.minecraft.gui.container.AbstractGuiOverlay$UserInputGuiScreen");
         defaultExclusions.add("ai.arcblroth.projectInception.client.InceptionInterfaceScreen");
+        defaultExclusions.add("net.optifine.gui.GuiChatOF");
         BlurConfig.init("blur", BlurConfig.class);
 
         ShaderEffectRenderCallback.EVENT.register((deltaTick) -> {
